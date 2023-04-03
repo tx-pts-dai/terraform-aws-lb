4095410d2ff4cd195284c29efa33e9ba3cad85e3
David B <38657416+Davidoutz@users.noreply.github.com>
2023-04-03 12:08:35 +0200

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -3,23 +3,23 @@ output "load_balancer_dns_name" {
   value       = aws_lb.app.dns_name
 }
 
-output "load_balancer_arn_suffix" {
-  description = "The ARN suffix of the load balancer."
-  value       = aws_lb.app.arn_suffix
-}
-
-output "load_balancer_arn" {
-  description = "The ARN sffix of the load balancer."
-  value       = aws_lb.app.arn
-}
-
-
-output "target_group_arns" {
-  description = "A list of ARNs for all target groups associated with the load balancer."
-  value       = aws_lb_target_group.app[*].arn
-}
-
-output "target_group_arns_suffix" {
-  description = "A list of ARNs suffix for all target groups associated with the load balancer."
-  value       = aws_lb_target_group.app[*].arn_suffix
-}
+#output "load_balancer_arn_suffix" {
+#  description = "The ARN suffix of the load balancer."
+#  value       = aws_lb.app.arn_suffix
+#}
+#
+#output "load_balancer_arn" {
+#  description = "The ARN sffix of the load balancer."
+#  value       = aws_lb.app.arn
+#}
+#
+#
+#output "target_group_arns" {
+#  description = "A list of ARNs for all target groups associated with the load balancer."
+#  value       = aws_lb_target_group.app[*].arn
+#}
+#
+#output "target_group_arns_suffix" {
+#  description = "A list of ARNs suffix for all target groups associated with the load balancer."
+#  value       = aws_lb_target_group.app[*].arn_suffix
+#}
