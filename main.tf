59280b5dd41e3b1abd1694e970e7bb40377c21cf
David B <38657416+Davidoutz@users.noreply.github.com>
2023-04-03 14:04:22 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,4 @@
-terraform {
-  required_version = ">= 1.3.0"
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 4.0"
-    }
-  }
-}
+
 
 provider "aws" {
   region = "eu-central-1"
